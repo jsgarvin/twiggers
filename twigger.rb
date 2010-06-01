7a2f598bf89a0cdf42d845514f8033461db200b9
Jonathan Garvin <jgarvin@vostro.(none)>
2010-05-31 23:17:10 -0600

    change to use https for location query

--- twigger.rb
+++ twigger.rb
@@ -6,7 +6,7 @@ require 'open-uri'
 config = YAML.load_file("config.yml")
 
 # http://www.instamapper.com/api?action=getPositions&key=11030323885521488835&num=10&format=json
-page = open("http://www.instamapper.com/api?action=getPositions&key=#{config['imapper_key']}&num=10&format=json")
+page = open("https://www.instamapper.com/api?action=getPositions&key=#{config['imapper_key']}&num=10&format=json")
 doc = JSON page.read
 
 puts doc['positions'][0]['latitude']
